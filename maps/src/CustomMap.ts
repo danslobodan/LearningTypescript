a4cb5b33cfd859dfa8f58a64264619b808f9be33
Slobodan Dan <danastroman@gmail.com>
2021-02-22 23:13:23 +0100

    Added Mappable interface and addMarker method.

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,3 +1,10 @@
+export interface Mappable {
+    location : {
+        lat: number;
+        lng: number;
+    }
+}
+
 export class CustomMap {
 
     private googleMap: google.maps.Map;
@@ -10,5 +17,15 @@ export class CustomMap {
                 lng: 0
             }
         });
+    }
+
+    addMarker(mappable: Mappable) : void {
+        new google.maps.Marker({
+            map: this.googleMap,
+            position: {
+                lat: mappable.location.lat,
+                lng: mappable.location.lng
+            }
+        })
     }
 }
