6c9b4bf41d6bbfc7ba835ff18053d2f40a8b8f39
Slobodan Dan <danastroman@gmail.com>
2021-02-24 21:19:18 +0100

    Added useEffect hook for UserSearch demoing ref.

--- rts/src/refs/UserSearch.tsx
+++ rts/src/refs/UserSearch.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const users = [
     { name: 'Sarah', age: 20 },
@@ -8,9 +8,17 @@ const users = [
 
 const UserSearch: React.FC = () => {
 
-    const inputRef = useRef();
+    const inputRef = useRef<HTMLInputElement | null>(null);
     const [name, setName] = useState('');
     const [user, setUser] = useState<{ name: string, age: number} | undefined>();
+
+    useEffect(() => {
+        if (!inputRef.current) {
+            return;
+        }
+
+        inputRef.current.focus()
+    },[])
 
     const onClick = () => {
         const foundUser = users.find(user => {
@@ -22,7 +30,7 @@ const UserSearch: React.FC = () => {
 
     return <div>
         UserSearch
-        <input value={name} onChange={e => setName(e.target.value)} />
+        <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
         <button onClick={onClick}>Find User</button>
         <div>
             {user && user.name}
