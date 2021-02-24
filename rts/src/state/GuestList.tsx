954ac9190a4ac383cc5accef9fac9fb3718add79
Slobodan Dan <danastroman@gmail.com>
2021-02-24 20:33:46 +0100

    Added input, button and state name.

--- rts/src/state/GuestList.tsx
+++ rts/src/state/GuestList.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
+
 const GuestList: React.FC = () => {
+
+    const [name, setName] = useState('');
+
     return <div>
-        Guest List
+        <h3>Guest List</h3>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <button>Add Guest</button>
     </div>
 }
 
