2960b653bf57aaa195d14fe357ab88c0e75800a4
Slobodan Dan <danastroman@gmail.com>
2021-02-24 20:56:12 +0100

    Added a type annotation to a standalone event.

--- rts/src/events/EventComponent.tsx
+++ rts/src/events/EventComponent.tsx
@@ -1,6 +1,11 @@
 const EventComponent: React.FC = () => {
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event);
+    }
+
     return <div>
-        <input onChange={e => console.log(e)} />
+        <input onChange={onChange} />
     </div>
 }
 
